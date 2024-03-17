caab8f59a85c65b68c493286e0904d5411e3ecbc
puzzlecollector <puzzlecollector1123581321@gmail.com>
2024-03-17 16:30:43 +0900

    static problem

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,8 @@ from .base import *
 
 ALLOWED_HOSTS = ["43.200.37.42"]
 
-STATIC_ROOT = BASE_DIR / "static/"
+# STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = "/home/ubuntu/projects/mysite/staticfiles/"
 STATICFILES_DIRS = []
 DEBUG = True
 
