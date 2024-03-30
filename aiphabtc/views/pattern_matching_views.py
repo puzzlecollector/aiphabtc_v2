d2d0cd4074c7b9068b8ae9a090f885104ad3508d
puzzlecollector <puzzlecollector1123581321@gmail.com>
2024-03-31 00:48:44 +0900

    news pattern matching functionality prototype

--- aiphabtc/views/pattern_matching_views.py
+++ aiphabtc/views/pattern_matching_views.py
@@ -35,14 +35,13 @@ from common.models import PointTokenTransaction
 from django.db.models import Q
 from django.core.cache import cache
 import backoff
-import faiss
+from annoy import AnnoyIndex # alternative to faiss-cpu
 import json
-import threading
+import gc
 
 # prepare model and tokenizer
 tokenizer = AlbertTokenizer.from_pretrained("aiphabtc/kr-cryptodeberta")
 embedding_model = AutoModel.from_pretrained("aiphabtc/kr-cryptodeberta")
-config = AutoConfig.from_pretrained("aiphabtc/kr-cryptodeberta")
 device = torch.device("cpu") # default to CPU
 
 # read candidate texts
@@ -53,18 +52,19 @@ with open('aiphabtc/candidate_texts_0325.pkl', 'rb') as f:
 with open('aiphabtc/published_datetimes_0325.pkl', 'rb') as f:
     published_datetimes = pickle.load(f)
 
-# get faiss index
-index = faiss.read_index('aiphabtc/coinness_faiss_index_0325.index')
-
 # get chart data
-chart_df = pd.read_csv("aiphabtc/upbit_chart_data_0325.csv")
+chart_df = pd.read_csv("aiphabtc/upbit_chart_data_0325_v2.csv")
 
 def inner_product_to_percentage(inner_product):
     return (inner_product + 1) / 2 * 100
 
+# get annoy index
+u = AnnoyIndex(768, 'angular')
+# Load the saved index
+u.load('aiphabtc/coinness_annoy_30.index')  # Path to saved index
+
 def get_query_embedding(query):
-    encoded_query = tokenizer(query, max_length=512, padding="max_length", truncation=True, return_tensors="pt").to(device)
-    embedding_model.to(device)
+    encoded_query = tokenizer(query, max_length=512, padding="max_length", truncation=True, return_tensors="pt")
     embedding_model.eval()
     with torch.no_grad():
       query_embedding = embedding_model(**encoded_query)[0][:, 0, :]
@@ -72,23 +72,31 @@ def get_query_embedding(query):
     return query_embedding
 
 def get_relevant_chart_segment1d(chart_df, datestr):
-    chart_df.set_index(pd.DatetimeIndex(chart_df.index), inplace=True)
+    print(chart_df)
+    chart_df.set_index(pd.DatetimeIndex(chart_df["dates"]), inplace=True)
     df1d_idx = -1
-    cur_date = chart_df.index.values  # Ensure this column contains date and time
-    news_datestr = datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S")
+    cur_date = chart_df["dates"].values  # Ensure this column contains date and time
+    news_datestr = datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S") 
     for i in range(len(cur_date) - 1):
         # Convert numpy.datetime64 to string and then to datetime
-        current_date_str = cur_date[i].astype(str)
-        current_date_str = current_date_str.split('T')[0] + ' ' + current_date_str.split('T')[1].split('.')[0]
+        current_date_str = cur_date[i]
+        # current_date_str = current_date_str.split('T')[0] + ' ' + current_date_str.split('T')[1].split('.')[0]
         current_date = datetime.strptime(current_date_str, "%Y-%m-%d %H:%M:%S")
-        next_date_str = cur_date[i + 1].astype(str)
-        next_date_str = next_date_str.split('T')[0] + ' ' + next_date_str.split('T')[1].split('.')[0]
+        next_date_str = cur_date[i + 1]
+        # next_date_str = next_date_str.split('T')[0] + ' ' + next_date_str.split('T')[1].split('.')[0]
         next_date = datetime.strptime(next_date_str, "%Y-%m-%d %H:%M:%S")
         if news_datestr >= current_date and news_datestr < next_date:
             df1d_idx = i
             break
     return df1d_idx
 
+def distance_to_percentage(distances):
+    # Convert squared Euclidean distances to cosine similarity percentages
+    percentages = [(1 - (d**2) / 2.0) * 50 + 50 for d in distances]
+    return percentages
+
+
+# use annoy instead of faiss for fast vector similarity search
 def search_news(request):
     if request.method == "POST":
         data = json.loads(request.body)
@@ -96,15 +104,16 @@ def search_news(request):
         topk = int(data.get("top_k", 5))
         topk = max(5, min(topk, 20))
         # call the functions to perform the search
-        query_embedding = get_query_embedding(query) 
-        faiss.normalize_L2(query_embedding)
-        distances, indices = index.search(query_embedding, 1000)
+        query_embedding = get_query_embedding(query)
+        query_embedding = query_embedding.reshape((768))
+        indices, distances = u.get_nns_by_vector(query_embedding, 30, include_distances=True)  # Finds the 30 nearest neighbors
+        percentages = distance_to_percentage(distances)
         results = []
         for i in range(topk):
-            text = candidate_texts[indices[0][i]]
+            text = candidate_texts[indices[i]]
             text = text.replace("\n", "<br>")
-            similarity = round(inner_product_to_percentage(distances[0][i]), 3)
-            date = published_datetimes[indices[0][i]]
+            similarity = round(percentages[i], 3)
+            date = published_datetimes[indices[i]]
             relevant_chart_idx_1d = get_relevant_chart_segment1d(chart_df, date)
             relevant_chart_segment_1d = chart_df.iloc[relevant_chart_idx_1d:relevant_chart_idx_1d + 30] # relevant chart data for the next 1 month
 
@@ -119,7 +128,6 @@ def search_news(request):
                 "date": date,
                 "chart_data_1d": chart_data_1d,
             })
-
 
         # Return the results as Json
         return JsonResponse({"results": results})
--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE' # temporary fix for conflicting FAISS and pytorch OpenMP
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
