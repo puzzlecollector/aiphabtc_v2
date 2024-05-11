8b7be04de69442e64a9fd5e293b38debec4b2966
puzzlecollector <puzzlecollector1123581321@gmail.com>
2024-05-11 18:39:15 +0900

    added question list under question details

--- aiphabtc/views/base_views.py
+++ aiphabtc/views/base_views.py
@@ -525,10 +525,47 @@ def index_orig(request, board_name="free_board"):
     }
     return render(request, 'aiphabtc/question_list.html', context)
 
-
+'''
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
+    return render(request, 'aiphabtc/question_detail.html', context)
+'''
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
+    so = request.GET.get("so", "recent")
+
+    question_list = Question.objects.filter(board=question.board)
+
+    if so == "recommend":
+        question_list = question_list.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == "popular":
+        question_list = question_list.annotate(num_answer=Count("answer")).order_by("-num_answer", "-create_date")
+    else:
+        question_list = question_list.order_by("-create_date")
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+
+    paginator = Paginator(question_list, 10)  # Show 10 questions per page
+    page_obj = paginator.get_page(page)
+
+    context = {
+        "question": question,
+        "question_list": page_obj,
+        "board": question.board,
+        'page': page,
+        'kw': kw,
+        'so': so
+    }
     return render(request, 'aiphabtc/question_detail.html', context)
 
 
